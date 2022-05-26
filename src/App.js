c01e551830e55f255a65e690d094d750f623a6dc
Shivam Rathod <rathodshivam05@gmail.com>
2022-05-26 09:06:43 +0530

    Tic-toc-toe

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Tictactao } from './components/Tictactao';
+
+function App() {
+  return (
+    <div className="App">
+      <Tictactao/>
+    </div>
+  );
+}
+
+export default App;
